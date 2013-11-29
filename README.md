10b69fc8df43ed47140ef0759d0ac0626e20c0dd
JR Liwag <liwagr@gmail.com>
2013-11-30 02:06:32 +0400

    Added master and worker files. Added package.json.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ serverrunner
 ============
 
 serverrunner starts node servers and then keeps them running. Allows for graceful shutdowns.
+
+
+
+Next Features
+============
+  * Graceful restart
+  * Multiple servers and ports
+  * Selective shutdown/restart of workers
+
---
+++ index.js
@@ -0,0 +1,63 @@
+var forever = require('forever-monitor'),
+    express = require('express'),
+    path = require('path'),
+    watchr = require('watchr');
+
+/**
+ * @public
+ * @cfg {Number} workers Number of workers to create.
+ * @cfg {String} server Path to server file.
+ * @cfg {Number} port Port number to start serving from.
+ * @cfg {String} [watch] Directory to watch for changes. Workers will restart if
+ * any files in this directory change.
+ */
+module.exports = function (config) {
+    var workers = [],
+        numWorkers = config.workers,
+        serverFile = config.server,
+        startPort = config.port,
+        watchDir = config.watch,
+        cowboyWorker = path.resolve(__dirname, 'lib/worker.js'),
+        worker,
+        index;
+
+    function restartWorkers() {
+        console.log('Restarting workers.');
+        workers.forEach(function (worker) {
+            worker.restart();
+        });
+    }
+
+    function shutdownWorkers() {
+        console.log('Shutting down workers.');
+        workers.forEach(function (worker) {
+            worker.stop();
+        });
+    }
+
+    for (index = 0; index < numWorkers; index += 1) {
+        worker = new forever.Monitor(cowboyWorker, {
+            silent: false,
+            killTree: true,
+            options: ['--port', startPort + index, '--server', serverFile],
+        });
+
+        workers.push(worker);
+        worker.start();
+    }
+
+    if (watchDir !== undefined) {
+        watchr.watch({
+            path: watchDir,
+            listener: function(event, filename) {
+                console.log(filename + ' changed.');
+                restartWorkers();
+            }
+        });
+    }
+
+    process.on('SIGINT', function () {
+        shutdownWorkers();
+        process.exit();
+    });
+};
